51d9133a3658cdafa9c421f0c723e83c47e892d8
zeeshanthedev590 <62704414+zeeshanthedev590@users.noreply.github.com>
2020-12-11 14:27:56 +0500

    Update main.py

--- Water Notifications/main.py
+++ Water Notifications/main.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     notification.notify(
         title = 'Please Drink Water Zeeshan',
         message = 'Drink Some Water',
-        app_icon = 'E:\My projects\Python Projects\Water Notifications\water.ico',
+        app_icon = 'G:\My projects\Python Projects\Water Notifications\water.ico',
         timeout= 12
     )
     time.sleep(60*60)
